684b6e488b277dcc4e31c870d3ef0f3de319cf27
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-08-26 19:58:03 -0500

    get user by id

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^media/$', views.all_media, name="All media"),
     url(r'^users/$', views.all_users, name="All users"),
+    url(r'^user/(?P<user_id>\d+)/$', views.user_by_id, name="User by id"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -4,7 +4,7 @@ from gallery.models import Media
 from gallery.models import User
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers as jsonserializer
-
+from django.http import Http404
 
 def all_media(request):
     all_media_objects = Media.objects.all()
@@ -16,3 +16,10 @@ def all_users(request):
     all_users_objects = User.objects.all()
 
     return HttpResponse(jsonserializer.serialize("json", all_users_objects))
+
+def user_by_id(request, user_id):
+    try:
+        user = User.objects.filter(idUser=user_id)
+    except User.DoesNotExist:
+        raise Http404("User does not exist.")
+    return HttpResponse(jsonserializer.serialize("json", user))
